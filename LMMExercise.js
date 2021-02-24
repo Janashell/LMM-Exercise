c0e3f01376e63f598d9860117f6f1a892cb65935
Janashell <janashell07@gmail.com>
2021-02-24 13:02:25 -0500

    LMM Exercise

---
+++ LMMExercise.js
@@ -0,0 +1,37 @@
+// 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+console.log(warmHugs);
+
+// 1b
+console.log(warmHugs.toUpperCase());
+
+// 1c
+console.log(warmHugs.replace("like", "love"));
+warmHugs = "Hi, I'm Olaf and I love warm hugs";
+
+// 2a
+let beenImpaled = "Oh, look at that. I've been impaled"
+console.log(beenImpaled);
+
+// 2b
+console.log(beenImpaled.slice(17, 35));
+
+//  3a
+let dotDotDot = "..."
+
+// 3b
+
+// 4
+console.log(Math.PI);
+
+// 5
+let randomNumber = Math.random();
+randomNumber *= 3;
+randomNumber = Math.floor(randomNumber);
+randomNumber ++;
+console.log(randomNumber);
+
+// BONUS
+
+// 6
+
